c4e7fd856da96473a2da86b8134db7f6e39bf9c9
Smith06S <sarahelenasmith@.com>
2026-04-28 16:35:22 +0200

    messagerie model

---
+++ models/file_discussion.py
@@ -0,0 +1,21 @@
+class FileDiscussion:
+    """Représente une agence (Entité)"""
+    def __init__(self, id_file_discussion, nom_discussione, date_creation):
+        self.id_file_discussion = id_file_discussion
+        self.nom_discussione = nom_discussione
+        self.date_creation = date_creation
+
+class FileDiscussionRepository:
+    """Gère la communication avec la table 'file discussion'"""
+    def __init__(self, db_connexion):
+        self.db = db_connexion
+
+    def find_all(self):
+        cur = self.db.cursor()
+        cur.execute("SELECT id_file_discussion, nom_discussione, date_creation FROM file_discussion")
+        rows = cur.fetchall()
+
+        file_discussions = [FileDiscussion(r[0], r[1], r[2]) for r in rows]
+
+        cur.close()
+        return file_discussions
--- models/messagerie.py
+++ models/messagerie.py
@@ -1,20 +1,21 @@
 class Messagerie:
     """Représente une messagerie (Entité)"""
-    def __init__(self, id_messagerie, message, date_heure, id_commercial, id_client):
+    def __init__(self, id_messagerie, message, date_heure, id_commercial, id_client, id_file_discussion):
         self.id_messagerie = id_messagerie
         self.message = message
         self.date_heure = date_heure
         self.id_commercial = id_commercial
         self.id_client = id_client
+        self.id_file_discussion = id_file_discussion
 
 class MessagerieRepository:
     """Gère la communication avec la table 'messagerie'"""
     def __init__(self, db_connexion):
         self.db = db_connexion
 
-    def find_all(self):
+    def find_all(self, id_file_discussion):
         cur = self.db.cursor()
-        cur.execute("SELECT id_messagerie, message, date_heure, id_commercial, id_client FROM messagerie")
+        cur.execute("SELECT id_messagerie, message, date_heure, id_commercial, id_client FROM messagerie WHERE id_file_discussion = %s", (id_file_discussion,))
         rows = cur.fetchall()
 
         messageries = [Messagerie(r[0], r[1], r[2], r[3], r[4]) for r in rows]
