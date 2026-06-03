70963571df0e708370da3df97f27ab224a49396d
Smith06S <sarahelenasmith@.com>
2026-06-03 17:53:20 +0200

    chore favoris feature

--- models/agence.py
+++ models/agence.py
@@ -1,10 +1,11 @@
 class Agence:
-    def __init__(self, id_agence, nom, ville, adresse, image_url=None):
+    def __init__(self, id_agence, nom, ville, adresse, image_url=None, nb_favoris=0):
         self.id_agence = id_agence
         self.nom = nom
         self.ville = ville
         self.adresse = adresse
         self.image_url = image_url or 'static/images/default_agence.jpg'
+        self.nb_favoris = nb_favoris # Vous pouvez maintenant stocker le nombre de favoris !
 
 class AgenceRepository:
     def __init__(self, db_connection):
@@ -32,4 +33,11 @@ class AgenceRepository:
                 ORDER BY nb_favoris DESC
                 LIMIT 5
             """)
-            return [Agence(**row) for row in cur.fetchall()]
+            
+            agences = []
+            for row in cur.fetchall():
+                row_dict = dict(row)
+                row_dict.pop('nb_favoris', None) 
+                agences.append(Agence(**row_dict))
+                
+            return agences
