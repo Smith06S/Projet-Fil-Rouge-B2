0a1642bd91ff7d7c7889f9aa593e1aa526eb3a89
Xelea <campello.juliette@gmail.com>
2026-06-07 15:03:21 +0200

    filter2

--- controllers/bien_controller.py
+++ controllers/bien_controller.py
@@ -56,19 +56,7 @@ def detail_bien(id_bien):
 def voir_carte_bien(id_bien):
     conn = db_manager.get_connection()
     repo = BienRepository(conn)
-    
-    from_agence = False
-    id_agence = None
-
-    if id_bien == 0:
-        id_agence = request.args.get('agence')
-        from models.agence import AgenceRepository
-        repo_agence = AgenceRepository(conn)
-        bien_obj = repo_agence.get_by_id(id_agence)
-        bien_obj = {'adresse': bien_obj.adresse, 'ville': bien_obj.ville, 'id_agence': bien_obj.id_agence, 'id_bien': 0}
-        from_agence = True
-    else:
-        bien_obj = repo.get_by_id(id_bien)
+    bien_obj = repo.get_by_id(id_bien)
     
     if not bien_obj:
         conn.close()
@@ -78,7 +66,7 @@ def voir_carte_bien(id_bien):
     carte_html = generer_carte_un_bien(bien_obj, conn)
     conn.close()
     
-    return render_template('carte_bien.html', bien=bien_obj, carte_html=carte_html, from_agence=from_agence, id_agence=id_agence)
+    return render_template('carte_bien.html', bien=bien_obj, carte_html=carte_html)
 
 # Définir les extensions autorisées
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp', 'gif'}
