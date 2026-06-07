7a874cce7f447d0992cda8ea316f8594d81a52e2
Xelea <campello.juliette@gmail.com>
2026-06-07 17:37:48 +0200

    carte13

--- models/carte.py
+++ models/carte.py
@@ -27,11 +27,11 @@ def generer_carte_un_bien(bien_principal, db_connection):
         lat, lon = obtenir_coordonnees(b['adresse'], b['ville'])
         
         if est_le_bien_principal:
-            couleur_pin = "red"
-            icone_style = "star"
+            couleur_icone = "#534E47" 
+            icone_type = "fa-star"
         else:
-            couleur_pin = "blue"
-            icone_style = "home"
+            couleur_icone = "#918575" 
+            icone_type = "fa-home"
             
         lien_detail = url_for('bien.detail_bien', id_bien=b['id_bien'])
         popup_content = f"""
@@ -45,11 +45,15 @@ def generer_carte_un_bien(bien_principal, db_connection):
                 </a>
             </div>
             """
+        html_icon = f"""
+            <div style="color: {couleur_icone}; font-size: 24px; text-shadow: 0px 0px 3px white;">
+                <i class="fa {icone_type}"></i>
+            </div>"""
         
         folium.Marker(
             location=[lat, lon],
             popup=folium.Popup(popup_content, max_width=300),
-            icon=folium.Icon(color=couleur_pin, icon=icone_style, prefix="fa")
+            icon=folium.DivIcon(html=html_icon)
         ).add_to(m)
         
     return m._repr_html_()
