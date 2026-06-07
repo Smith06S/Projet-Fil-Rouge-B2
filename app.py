89385d073934dde769c672597c9307e0adbe0da8
Smith06S <sarahelenasmith@.com>
2026-06-07 11:31:43 +0200

    removed unecessary comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from controllers.auth_controller import auth_bp
 from controllers.agence_controller import agence_bp
 from controllers.bien_controller import bien_bp
@@ -17,6 +17,10 @@ app.register_blueprint(dashboard_bp)
 @app.route('/ymmo')
 def ymmo_global_redirect():
     return redirect(url_for('agence.accueil'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page404.html'), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
