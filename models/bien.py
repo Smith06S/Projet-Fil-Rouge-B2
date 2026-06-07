75f4ca7a70f463c6c3e8c5e42f828e572eaaa8e4
Xelea <campello.juliette@gmail.com>
2026-06-07 18:44:57 +0200

    profil3

--- models/bien.py
+++ models/bien.py
@@ -114,7 +114,13 @@ class BienRepository:
     def get_favoris_by_client(self, id_client):
         with self.db.cursor() as cur:
             cur.execute("SELECT b.* FROM bien b JOIN favoris f ON b.id_bien = f.id_bien WHERE f.id_client = %s", (id_client,))
-            return [Bien(**row) for row in cur.fetchall()]
+            biens = []
+            for row in cur.fetchall():
+                bien = Bien(**row)
+                cur.execute("SELECT image_url FROM photo_bien WHERE id_bien = %s", (bien.id_bien,))
+                bien.images = [p['image_url'] for p in cur.fetchall()]
+                biens.append(bien)
+            return biens
         
     def update(self, id_bien, ville, adresse, description, nbr_pieces, surface, type_bien, prix, nbr_chambres, a_balcon, a_parking, type_chauffage, etage, a_ascenseur, etat_logement, annee_construction):
         with self.db.cursor() as cur:
