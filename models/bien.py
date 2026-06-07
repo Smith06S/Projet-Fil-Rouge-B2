34e986aab4e7c970ad34805cbafddb698d314c7a
Xelea <campello.juliette@gmail.com>
2026-06-07 14:50:14 +0200

    filter

--- models/bien.py
+++ models/bien.py
@@ -101,7 +101,9 @@ class BienRepository:
             cur.execute(query, params)
             biens = []
             for row in cur.fetchall():
-                bien = Bien(**row)
+                bien_dict = dict(row)
+                bien = Bien(**bien_dict)
+                
                 cur.execute("SELECT image_url FROM photo_bien WHERE id_bien = %s", (bien.id_bien,))
                 bien.images = [p['image_url'] for p in cur.fetchall()]
                 biens.append(bien)
